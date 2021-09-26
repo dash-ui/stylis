655fde83163f0faa2eea2ee6c0f856ed4f77730d
Jared Lunde <jared.lunde@gmail.com>
2021-09-26 17:00:12 -0600

    chore: use pnpm and github actions

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-<hr>
-  <br/>
-  <img src='https://github.com/dash-ui/styles/raw/master/assets/logo.png'/>
-  <blockquote>
-    A tinier build of <a href="https://github.com/thysultan/stylis.js">Stylis</a>
-  </blockquote>
-  <pre>npm i @dash-ui/stylis</pre>
-  <br/>
-  
+<hr/>
+
+<img src='https://github.com/dash-ui/styles/raw/main/assets/logo.png'/>
+
+> A fork of @emotion/stylis that makes it even smaller, still
+
+```sh
+npm i @dash-ui/stylis
+```
+
+<p>
   <a href="https://bundlephobia.com/result?p=@dash-ui/stylis">
     <img alt="Bundlephobia" src="https://img.shields.io/bundlephobia/minzip/@dash-ui/stylis?style=for-the-badge&labelColor=24292e">
   </a>
@@ -16,8 +18,8 @@
   <a aria-label="Code coverage report" href="https://codecov.io/gh/dash-ui/stylis">
     <img alt="Code coverage" src="https://img.shields.io/codecov/c/gh/dash-ui/stylis?style=for-the-badge&labelColor=24292e">
   </a>
-  <a aria-label="Build status" href="https://travis-ci.com/dash-ui/stylis">
-    <img alt="Build status" src="https://img.shields.io/travis/com/dash-ui/stylis?style=for-the-badge&labelColor=24292e">
+  <a aria-label="Build status" href="https://github.com/dash-ui/stylis/actions/workflows/release.yml">
+    <img alt="Build status" src="https://img.shields.io/github/workflow/status/dash-ui/stylis/release/main?style=for-the-badge&labelColor=24292e">
   </a>
   <a aria-label="NPM version" href="https://www.npmjs.com/package/@dash-ui/stylis">
     <img alt="NPM Version" src="https://img.shields.io/npm/v/@dash-ui/stylis?style=for-the-badge&labelColor=24292e">
@@ -25,7 +27,9 @@
   <a aria-label="License" href="https://jaredlunde.mit-license.org/">
     <img alt="MIT License" src="https://img.shields.io/npm/l/@dash-ui/stylis?style=for-the-badge&labelColor=24292e">
   </a>
-<hr>
+</p>
+
+---
 
 `@dash-ui/stylis` is a version of [Stylis](https://github.com/thysultan/stylis.js) that has been modified slightly to make it smaller. The only Stylis option that can be changed is `prefix`, the rest of the options are already set to the values shown below and cannot be changed. This package also only exports the constructer, so you have to do `new Stylis()` and use the result from that rather than directly calling `Stylis`. The result of that function also cannot be used to create a stylis instance unlike stylis.
 
@@ -38,15 +42,15 @@ type Options = {
   cascade: true,
   compress: false,
   prefix: boolean | ((key: string, value: string, context: number) => boolean),
-}
+};
 ```
 
 ```jsx
-import Stylis from '@dash-ui/stylis'
+import Stylis from "@dash-ui/stylis";
 
-const stylis = new Stylis()
+const stylis = new Stylis();
 
-stylis('.css-hash', 'display:flex;') // .css-hash{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;}
+stylis(".css-hash", "display:flex;"); // .css-hash{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;}
 ```
 
 ## Building this package
@@ -57,3 +61,7 @@ To build this package from a newer version of stylis, update the version of styl
 
 - The [emotion](https://github.com/emotion-js) team did most of the work on this one, I just did some extra minifying ❤️
 - Stylis was written by [Sultan Tarimo](https://github.com/thysultan). ❤️
+
+## LICENSE
+
+MIT
--- scripts/build.js
+++ scripts/build.js
@@ -1,20 +1,20 @@
 // Once again, max credit to the emotion team
 // https://github.com/emotion-js/emotion/blob/master/packages/stylis/build.js
-const stylisPath = require.resolve('stylis')
-const {promisify} = require('util')
-const fs = require('fs')
-const j = require('jscodeshift')
-const request = require('request-promise-native')
-const prettier = require('prettier')
+const stylisPath = require.resolve("stylis");
+const fs = require("fs");
+const { promisify } = require("util");
+const j = require("jscodeshift");
+const prettier = require("prettier");
 // eslint-disable-next-line import/no-extraneous-dependencies
-const recast = require('recast')
+const recast = require("recast");
+const request = require("request-promise-native");
 
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 const simplifySet = (src) =>
   j(src)
-    .find(j.FunctionDeclaration, {id: {name: 'set'}})
+    .find(j.FunctionDeclaration, { id: { name: "set" } })
     .forEach((path) => {
       j(path).replaceWith(
         recast.parse(`function set(options) {
@@ -32,137 +32,137 @@ const simplifySet = (src) =>
         }
         return set
       }`).program.body[0]
-      )
+      );
     })
-    .toSource()
+    .toSource();
 
 const removeOptions = (src) =>
   j(src)
     .find(j.SwitchStatement)
     .forEach((thing) => {
       thing.value.cases = thing.value.cases.filter((node) => {
-        if (!node.test) return true
+        if (!node.test) return true;
         switch (node.test.value) {
-          case 'keyframe':
-          case 'cascade':
-          case 'preserve':
-          case 'semicolon':
-          case 'compress':
-          case 'global': {
-            return false
+          case "keyframe":
+          case "cascade":
+          case "preserve":
+          case "semicolon":
+          case "compress":
+          case "global": {
+            return false;
           }
           default: {
-            return true
+            return true;
           }
         }
-      })
+      });
     })
-    .toSource()
+    .toSource();
 
 const setOptions = (src) =>
   j(src)
     .find(j.VariableDeclarator)
     .forEach((path) => {
       switch (path.value.id.name) {
-        case 'escape':
-        case 'keyed': {
-          path.value.init.value = 0
-          return
+        case "escape":
+        case "keyed": {
+          path.value.init.value = 0;
+          return;
         }
-        case 'semicolon': {
-          path.value.init.value = 1
+        case "semicolon": {
+          path.value.init.value = 1;
         }
       }
     })
-    .toSource()
+    .toSource();
 
 const removeUMDWrapper = (src) => {
-  let code
+  let code;
   j(src)
-    .find(j.FunctionExpression, {id: {name: 'factory'}})
+    .find(j.FunctionExpression, { id: { name: "factory" } })
     .forEach((path) => {
-      code = j(path).toSource()
+      code = j(path).toSource();
     })
-    .toSource()
-  return "'use strict';\nwindow['stylis'] = " + code
-}
+    .toSource();
+  return "'use strict';\nwindow['stylis'] = " + code;
+};
 
 async function doThing() {
   const stylisSrc = (await readFile(stylisPath))
     .toString()
     .replace(
-      'cascade + quote + bracket + atrule === 0 && id !== KEYFRAME && code !== SEMICOLON',
-      'true === false'
+      "cascade + quote + bracket + atrule === 0 && id !== KEYFRAME && code !== SEMICOLON",
+      "true === false"
     )
     .replace(
-      'comment + quote + parentheses + bracket + semicolon === 0',
-      'true === false'
+      "comment + quote + parentheses + bracket + semicolon === 0",
+      "true === false"
     )
-    .replace('switch (selector.charCodeAt(1))', 'switch (100)')
-    .replace('(insert === 1)', '(true === false)')
-    .replace('input.charCodeAt(9)*keyed', '0')
-    .replace('switch (cascade + level) {', 'switch (2) {')
-    .replace('compress*code === 0', 'true')
-    .replace(`typeof(output = result) !== 'string'`, '(output = result)')
+    .replace("switch (selector.charCodeAt(1))", "switch (100)")
+    .replace("(insert === 1)", "(true === false)")
+    .replace("input.charCodeAt(9)*keyed", "0")
+    .replace("switch (cascade + level) {", "switch (2) {")
+    .replace("compress*code === 0", "true")
+    .replace(`typeof(output = result) !== 'string'`, "(output = result)")
     // .replace("stylis['set'] = set", '')
     // .replace('set(options)', '')
-    .replace('this !== void 0 && this.constructor === stylis', 'false')
-    .replace('return factory(selector)', '')
+    .replace("this !== void 0 && this.constructor === stylis", "false")
+    .replace("return factory(selector)", "");
   const result = removeUMDWrapper(
     simplifySet(setOptions(removeOptions(stylisSrc)))
-  )
+  );
   // await writeFile('./src/stylis.js', result)
-  console.log('start request')
+  console.log("start request");
   const data = (
-    await request('https://closure-compiler.appspot.com/compile', {
-      method: 'POST',
+    await request("https://closure-compiler.appspot.com/compile", {
+      method: "POST",
       form: {
         js_code: result,
-        compilation_level: 'ADVANCED_OPTIMIZATIONS',
-        output_format: 'text',
-        output_info: 'compiled_code',
+        compilation_level: "ADVANCED_OPTIMIZATIONS",
+        output_format: "text",
+        output_info: "compiled_code",
       },
     })
-  ).toString()
+  ).toString();
 
   let finalSrc = data
-    .replace(/([\w]+)\.charCodeAt\((.+?)\)/g, 'charCodeAt($1, $2)')
+    .replace(/([\w]+)\.charCodeAt\((.+?)\)/g, "charCodeAt($1, $2)")
     .replace(
       /([\w]+)\.replace\(([\w\s'"+-]+),([\w\s'"$:\-+-]+)\)/g,
-      'replace($1, $2, $3)'
+      "replace($1, $2, $3)"
     )
-    .replace(/([\w]+)\.replace\(([\w\s'"+-]+), ''\)/g, 'replace($1, $2, $3)')
-    .replace(/([\w]+)\.indexOf\(([^)]+?)\)/g, 'indexOf($1, $2)')
-    .replace(/([\w]+)\.substring\(([^)]+?)\)/g, 'substring($1, $2)')
+    .replace(/([\w]+)\.replace\(([\w\s'"+-]+), ''\)/g, "replace($1, $2, $3)")
+    .replace(/([\w]+)\.indexOf\(([^)]+?)\)/g, "indexOf($1, $2)")
+    .replace(/([\w]+)\.substring\(([^)]+?)\)/g, "substring($1, $2)");
 
   await writeFile(
-    './src/index.js',
+    "./src/index.js",
     top +
       prettier
         .format(finalSrc, {
           semi: false,
           singleQuote: true,
         })
-        .replace('window.stylis =', 'export default ')
-        .replace(/'-webkit-'/g, 'WEBKIT')
-        .replace(/'-ms-'/g, 'MS')
-        .replace(/'-moz-'/g, 'MOZ')
-        .replace(/'flex-'/g, 'FLEX')
+        .replace("window.stylis =", "export default ")
+        .replace(/'-webkit-'/g, "WEBKIT")
+        .replace(/'-ms-'/g, "MS")
+        .replace(/'-moz-'/g, "MOZ")
+        .replace(/'flex-'/g, "FLEX")
         .replace(/'([^']*?)-webkit-([^']*?)'/g, `'$1' + WEBKIT + '$2'`)
         .replace(/'([^']*?)-moz-([^']*?)'/g, `'$1' + MOZ + '$2'`)
         .replace(/'([^']*?)-ms-([^']*?)'/g, `'$1' + MS + '$2'`)
         .replace(/'([^']*?)flex-([^']*?)'/g, `'$1' + FLEX + '$2'`)
-        .replace(/'\$1'/g, '$$1')
-        .replace(/'\$2'/g, '$$2')
+        .replace(/'\$1'/g, "$$1")
+        .replace(/'\$2'/g, "$$2")
         .replace(/'\$1 \$2'/g, `$$1 + ' ' + $$2`)
-        .replace(/^([\s]+)'' \+/gm, '$1')
-        .replace(/return '' \+/gm, 'return')
-        .replace(/\+ '' \+/g, ' + ')
-        .replace(/'' \+/g, '')
-        .replace(/\+ ''/g, '')
-  )
+        .replace(/^([\s]+)'' \+/gm, "$1")
+        .replace(/return '' \+/gm, "return")
+        .replace(/\+ '' \+/g, " + ")
+        .replace(/'' \+/g, "")
+        .replace(/\+ ''/g, "")
+  );
 
-  console.log('done')
+  console.log("done");
 }
 
 const top = `
@@ -176,5 +176,5 @@ function charCodeAt (s, code) { return s.charCodeAt(code) }
 function replace (s, i, o) { return s.replace(i, o) }
 function indexOf (s, a, b) { return s.indexOf(a, b) }
 function substring (s, a, b) { return s.substring(a, b) }
-`
-doThing()
+`;
+doThing();
